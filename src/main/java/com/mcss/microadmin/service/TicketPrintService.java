e696ae1a1dbdff9e7cd0e74e05a1a9b548157879
oscdanrm <oscar.rangel@lunave.com>
2020-09-04 22:22:01 -0500

    Tickect

--- src/main/java/com/mcss/microadmin/service/TicketPrintService.java
+++ src/main/java/com/mcss/microadmin/service/TicketPrintService.java
@@ -7,9 +7,12 @@ package com.mcss.microadmin.service;
 
 import com.mcss.microadmin.data.dto.Store;
 import com.mcss.microadmin.data.dto.TicketData;
+import com.mcss.microadmin.data.entity.Order;
+import com.mcss.microadmin.data.entity.ProductOrder;
 import com.mcss.microadmin.data.entity.Sale;
 import com.mcss.microadmin.service.print.USBPrinter;
 import io.github.escposjava.print.Printer;
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
@@ -43,10 +46,48 @@ public class TicketPrintService {
         Printer printer = new USBPrinter(ticketData);
         io.github.escposjava.PrinterService ps = new io.github.escposjava.PrinterService(printer);
         
+        String products = getProducts(sale.getOrder().getOrder());
+        String direccion = "Av Madero Norte #25 Colonia Centro, CP:61940, Huetamo, Michoacan";
+        
         ps.init();
         ps.setTextAlignCenter();
         ps.lineBreak(3);
         ps.setTextSize2W();
         ps.printLn(store.getBussinesName());
+        ps.printLn(direccion);
+        ps.printLn("CONSUMO");
+        ps.printLn(products);
+        
+             
+    }
+    
+    public String getProducts(Order order){
+        
+        String productsString = "";
+        BigDecimal total = new BigDecimal(0);
+        
+        for(ProductOrder product : order.getProducts()) {
+            productsString += product.getProduct().getDescription();
+            if(product.getQuantity().compareTo(BigDecimal.ONE) >= 1){
+                productsString += "\n";
+                productsString += "     ";
+                productsString += product.getQuantity().toString()+" X $"+
+                        product.getProduct().getRetailPrice().toString();
+                productsString += ".......$"+product.getProduct().getRetailPrice().multiply(product.getQuantity());
+                productsString += "\n";
+                
+                total.add(product.getProduct().getRetailPrice().multiply(product.getQuantity()));
+                
+            }else{
+                productsString += "..........$"+product.getProduct().getRetailPrice();
+                productsString += "\n";
+                total.add(product.getProduct().getRetailPrice());
+            }
+        }
+        
+        productsString += "\n";
+        productsString += "TOTAL..........$ "+total.toString();
+        
+       return productsString;
     }
 }
