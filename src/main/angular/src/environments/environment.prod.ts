b0ee8f5206dc2824fac391013553b164db1f03b1
edgar <edgar@latitude>
2020-09-21 23:12:08 -0500

    Commit inicial

--- src/main/angular/src/environments/environment.prod.ts
+++ src/main/angular/src/environments/environment.prod.ts
@@ -1,8 +1,19 @@
+import { InjectableRxStompConfig } from '@stomp/ng2-stompjs';
+
 export const environment = {
   production: false,
   API_URL: '',
   user: '',
   password: '',
+  websocket: {
+    uri: 'ws://' + window.location.host + '/ws',
+    topicPrefix: "/web/action",
+    destination: '/updater/order',
+    process: {
+      topic: '/web/updater',
+      prefix: '/updater'
+    }
+  }
 };
 
 export const constants = {
@@ -11,4 +22,15 @@ export const constants = {
   TYPE_FILL: 1,
   TYPE_DOSIFICATION: 2,
   ORDER: 'order'
+}
+
+export const stompConfig: InjectableRxStompConfig = {
+  brokerURL: environment.websocket.uri,
+  connectHeaders: {
+    login: 'edgar',
+    passcode: 'p'
+  },
+  heartbeatIncoming: 0,
+  heartbeatOutgoing: 20000,
+  reconnectDelay: 200,
 }
